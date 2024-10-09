691aebd4790c8592adedd61fc1c9bc9fe2e77dab
Stephen Tung <stktung@yahoo.com>
2024-10-09 20:56:38 +0800

    Print more output in samples. Renamed var names for clarity.

--- sample_append.py
+++ sample_append.py
@@ -2,15 +2,25 @@ from esdbclient import EventStoreDBClient, NewEvent, StreamState
 
 client = EventStoreDBClient(uri="esdb://localhost:2113?tls=false")
 
+event_type = "SampleEvent"
 new_event = NewEvent(
-    type="some-event",
-    data=b'{"Id":"1", "value":"some data"}'
+    type="SampleEvent",
+    data=b'{"Id":"1", "importantData":"some value"}'
 )
 
+event_stream = "SampleStream"
 client.append_to_stream(
-    "SampleContent",
+    "SampleStream",
     events = [new_event],
     current_version = StreamState.ANY
 )
+
+print("************************");
+print("🎉 Congratulations, you have written an event!");
+print("Stream: " + event_stream);
+print("Event Type: " + event_type);
+print("Event Body: {\"id\":\"1\",\"importantData\":\"some value\"}");
+print("************************");
+
 client.close()
 
--- sample_read.py
+++ sample_read.py
@@ -2,14 +2,14 @@ from esdbclient import EventStoreDBClient, NewEvent, StreamState
 
 client = EventStoreDBClient(uri="esdb://localhost:2113?tls=false")
 
-
-
-events = client.get_stream("SampleContent")
+events = client.get_stream("SampleStream")
 
 for event in events:
-    print(event.data)
+    print("************************");
+    print("You have read an event!");
+    print("Stream: " + event.streamId);
+    print("Event Type: " + event.type);
+    print("Event Body: " + event.data);
+    print("************************");
 
-print("success")
 client.close()
-
-
