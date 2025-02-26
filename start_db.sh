a8ce64d11c19dfdbe5b29efebcd530a702b95393
tomthetrainer <tom@functionalmedia.com>
2025-02-25 22:07:41 -0500

    Start cluster patch (#2)
    * push shell script up, so I can grab a screenshot, DO NOT MERGE
    
    * patched pdf, should be ready to go, don't merge till tested
    
    * remove file
    
    * added file

--- start_db.sh
+++ start_db.sh
@@ -2,8 +2,33 @@
 ###########################################################
 #
 # Docker management script for Eventstore Training Classes
+# Version 1.0
+# Contributors: Tom Hanlon, Jean-Marc Julien, Stephen Tung
+#
+# Purpose: 
+# This script will run a docker container in a GitHub 
+# CodeSpace instance and that is it's primary purpose
+# When ran a second time it will kill the current instance and
+# Start a fresh instance, your data will be lost, this is 
+# intended behavior
+#
+# Secondary Purpose:
+# In an environment with docker installed this script should 
+# Function locally as well. 
 #
 ###########################################################
+
+
+###########################################################
+# Set EventStoreDB version
+# Version is set to whatever the LTS version is at
+# the time this script was last tested
+# If you wanted to manually change (at your own risk)
+# see, https://hub.docker.com/r/eventstore/eventstore/tags
+#
+###########################################################
+
+db_version=eventstore/eventstore:24.10.1
 
 #################################################################################
 # Some bash notes for the curious
@@ -65,7 +90,7 @@ docker run \
        -d \
        --name esdb-node \
        -p 2113:2113 \
-       eventstore/eventstore:lts \
+       $db_version \
        --insecure \
        --run-projections=All \
        --enable-atom-pub-over-http
